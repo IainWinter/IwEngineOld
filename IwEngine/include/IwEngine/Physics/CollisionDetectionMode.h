34975780c5e7b213328cc7f5bf14790573cbd190
Nat Adamian <nat.adamian241@gmail.com>
2017-12-19 17:53:15 -0500

    Finished Rigid Body and Collision detector

---
+++ IwEngine/include/IwEngine/Physics/CollisionDetectionMode.h
@@ -0,0 +1,4 @@
+#pragma once
+enum CollisionDetectionMode {
+	Continuous, Discrete
+};
--- IwEngine/include/IwEngine/Physics/RigidBody.h
+++ IwEngine/include/IwEngine/Physics/RigidBody.h
@@ -1,8 +1,15 @@
 #pragma once
+#include "CollisionDetectionMode.h"
+#include <tuple>
 
 class RigidBody {
-public: 
-	float mass; 
-	float drag; 
-
+public:
+	float mass;
+	float drag;
+	float angular_drag;
+	bool use_gravity;
+	bool is_kinematic;
+	CollisionDetectionMode collision_detection_mode;
+	std::tuple <bool, bool, bool> freeze_position_xyz;
+	std::tuple <bool, bool, bool> freeze_rotation;
 };
