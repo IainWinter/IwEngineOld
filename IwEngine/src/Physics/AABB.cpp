347d94da41938950710ba5028c0f580ceb5f53c6
Nat Adamian <nat.adamian241@gmail.com>
2017-12-31 17:45:12 -0500

    Finished AABB.h and .cpp

--- IwEngine/src/Physics/AABB.cpp
+++ IwEngine/src/Physics/AABB.cpp
@@ -6,5 +6,15 @@ Physics::CollisionData Physics::AABB::IntersectAABB(const AABB& other)
 	Math::Vector3 distance1 = other.min - max;
 	Math::Vector3 distance2 = min - other.max;
 	Math::Vector3 distance;
-	//Math::Vector3 = max distance1, distance2
+	float max_distance;
+	if (distance1.LengthFast() > distance2.LengthFast())
+	{
+		distance = distance1 - distance2;
+		max_distance = distance1.LengthFast();
+	}
+	else {
+		distance = distance2 - distance1; 
+		max_distance = distance2.LengthFast();
+	}
+	return CollisionData(max_distance < 0, max_distance);
 }
