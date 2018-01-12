d5547481db5f6c7e29fbebc71077b6aae7f94166
Nat Adamian <nat.adamian241@gmail.com>
2018-01-12 18:36:35 -0500

    f

--- IwEngine/src/ColliderSystem.cpp
+++ IwEngine/src/ColliderSystem.cpp
@@ -8,16 +8,7 @@
 
 void System<SphereCollider, BoxCollider>::Update(ComponentLookUp& componentLookUp, float deltaTime) {
 	ComponentTable <Transform>* transformTable = componentLookUp.GetComponentTable<Transform>();
-	ComponentTable <RigidBody>* rigidBodyTable = componentLookUp.GetComponentTable<RigidBody>();
 	std::vector<int> transformKeys = transformTable->GetGameObjectIDS();
-	std::vector<int> rigidBodyKeys = rigidBodyTable->GetGameObjectIDS();
-	std::vector<int> Objects; 
-	for (int i = 0; i < transformKeys.size(); i++){
-		for (int j = 0; j < rigidBodyKeys.size; j++){
-			if (transformKeys.at(i) = rigidBodyKeys.at(j)){
-				Objects.push_back(rigidBodyKeys.at(j));
-			}
-		}
-	}
+	
 }
 
