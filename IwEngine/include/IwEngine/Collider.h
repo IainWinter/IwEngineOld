36802d7ca9706500bc63649e916c774f42acf764
Nat Adamian <nat.adamian241@gmail.com>
2018-01-12 20:28:44 -0500

    f

--- IwEngine/include/IwEngine/Collider.h
+++ IwEngine/include/IwEngine/Collider.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "Component.h"
+#include "GameObject.h"
+#include <vector>
+#include "Math\Vector3.h"
+
+class Collider : public Component {
+private:
+	std::vector <Math::Vector3> points; 
+public:
+};
