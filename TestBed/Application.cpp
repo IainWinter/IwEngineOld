11c5bbbbe1c0604ccd9684207ba5dec7fc655931
IAINPC\Iain <iainwinter1@gmail.com>
2018-01-13 21:08:03 -0500

    fgfddfg

--- TestBed/Application.cpp
+++ TestBed/Application.cpp
@@ -17,14 +17,21 @@ int main() {
 	Scene& scene = engine->GetScene();
 
 	GameObject& gameObject = scene.MakeGameObject();
+	GameObject& gameObject2 = scene.MakeGameObject();
 
 	Graphics::Mesh* mesh = Graphics::Mesh::MakeBox(Math::Vector3(-0.5f), 1);
+	Graphics::Mesh* mesh2 = Graphics::Mesh::MakeBox(Math::Vector3(-0.5f), 0.5f);
 	
 	RenderMesh* renderMesh = new RenderMesh(gameObject, mesh);
+	RenderMesh* renderMesh2 = new RenderMesh(gameObject, mesh2);
 
 	gameObject.AddComponent<RenderMesh>(renderMesh);
 	gameObject.AddComponent<Transform>(new Transform(gameObject, Math::Vector3(0, 0, -2), Math::Quaternion::FromEulerAngles(0, 0.7853982f, 0)));
 	gameObject.AddComponent<RigidBody>(new RigidBody(gameObject));
+
+	gameObject2.AddComponent<RenderMesh>(renderMesh2);
+	gameObject2.AddComponent<Transform>(new Transform(gameObject2, Math::Vector3(0, 1, -2), Math::Quaternion::FromEulerAngles(0, 0.7853982f, 0)));
+	gameObject2.AddComponent<RigidBody>(new RigidBody(gameObject2));
 
 	scene.MakeSystem<RenderMesh, Transform>();
 	scene.MakeSystem<RigidBody, Transform>();
