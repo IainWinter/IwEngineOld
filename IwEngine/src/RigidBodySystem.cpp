746344fcc09f12fd2aef0c7dc068e49e36855445
Nat Adamian <nat.adamian241@gmail.com>
2018-01-13 20:53:49 -0500

    f

--- IwEngine/src/RigidBodySystem.cpp
+++ IwEngine/src/RigidBodySystem.cpp
@@ -22,15 +22,18 @@ void System<RigidBody, Transform>::Update(ComponentLookUp& componentLookUp, floa
 	for (size_t i = 0; i < gCount; i++) {
 		Transform* transform = componentLookUp.GetComponentTable<Transform>()->GetComponent(gameObjectIDs[i]);
 		RigidBody* rigidBody = componentLookUp.GetComponentTable<RigidBody>()->GetComponent(gameObjectIDs[i]);
-		Math::Vector3 position = transform->GetPosition;
-		Math::Quaternion rotation = transform->GetRotation;
+		Math::Vector3 position = transform->GetPosition();
+		Math::Quaternion rotation = transform->GetRotation();
 		if (rigidBody->use_gravity) {
 			Math::Vector3 gravity(0, -9.8f, 0);
 			rigidBody->force.operator+=(gravity);
 		}
 		//rigidBody->force.operator+=(applied force vector);
+		float frictionForce = rigidBody->material.coef_kinetic_friction*rigidBody->mass*rigidBody->velocity.y;
+		rigidBody->force.x += frictionForce;
 		Math::Vector3 acceleration = rigidBody->force.operator/=(rigidBody->mass);
-		position.operator+=(rigidBody->velocity.operator*(deltaTime)+);
+		position.operator+=(rigidBody->velocity.operator*=(deltaTime)+acceleration.operator/=(2).operator*=(deltaTime*deltaTime));
+		transform->SetPosition(position);
 		rigidBody->velocity.operator+=(acceleration.operator*(deltaTime));
 
 		transform->SetPosition(transform->GetPosition() - Math::Vector3(deltaTime, 0, 0));
