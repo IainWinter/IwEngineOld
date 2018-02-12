ad07916efb36c4869394229082474d08cc650bae
IAINPC\Iain <iainwinter1@gmail.com>
2018-02-11 21:54:15 -0500

    Tests

--- Tests/IwPhysicsTests/octreeTests.cpp
+++ Tests/IwPhysicsTests/octreeTests.cpp
@@ -19,6 +19,8 @@ namespace IwPhysicsTests {
 				iwphysics::boxCollider* boxCollider = new iwphysics::boxCollider(iwmath::vector3(x, y, z), iwmath::vector3::one);
 				octree.insert(boxCollider);
 			}
+
+			std::cout << "Done!";
 		}
 	};
 }
